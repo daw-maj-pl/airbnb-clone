86bba659478403cdf939061157284a421a508f3b
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-04-19 20:20:20 +0200

    Replace console.log with toast notification

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -2,6 +2,7 @@
 
 import axios from 'axios';
 import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
@@ -33,7 +34,7 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch(error => {
-        console.log(error);
+        toast.error('Something went wrong.');
       })
       .finally(() => {
         setIsLoading(false);
